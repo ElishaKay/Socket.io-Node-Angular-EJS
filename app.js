eb61102cc4380e1cdcedb06d9f1f7f00506e418c
Elisha Kay <kramer1346@gmail.com>
2017-12-05 20:19:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+// development only
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+}
+
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+};
+
+
+/**
+ * Routes
+ */
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.ejs')
+});
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Socket.io Communication
+io.sockets.on('connection', require('./routes/socket'));
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log("connected: % of sockets connected", connections.length);
+
+    //Disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected % of sockets connected', connections.length);        
+    });
+
+    //Message
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+    
+    // new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users)
+    } 
+});
+
+/**
+ * Start Server
+ */
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
